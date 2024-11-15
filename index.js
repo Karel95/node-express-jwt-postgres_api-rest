d929a57980f51085907cefd0ac5f9f438a6932fb
Karel <karelh2207@gmail.com>
2024-11-15 10:08:07 -0300

    adding cors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/user.route.js';
 
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' }));
+//app.use(cors({ origin: ['http://localhost:5173', 'http://otro-dominio.com'] }));
+//app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
